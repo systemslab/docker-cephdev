8d768dc73a287405f797266392206c62bd5db2c0
Ivo Jimenez <ivo.jimenez@gmail.com>
2017-04-17 19:54:05 -0700

    Adds way of getting libltdl path

--- aliases.sh
+++ aliases.sh
@@ -2,11 +2,16 @@
 #  * add way of identifying cephbuilder image tag based on HEAD
 #  * if no args given, select ivotron/cephbuilder:jewel
 function dmake {
-  docker run --rm -ti --privileged \
+  docker_path=$(which docker)
+  libltdl_path=$(ldd /usr/bin/docker | grep libltdl | awk '{print $3}')
+
+  if [ -n "$libltdl_path" ] ; then
+    libltdl_path="--volume $libltdl_path:/usr/lib/$(basename $libltdl_path)"
+  fi
+
+  docker run --rm $libtdl_path \
     -v `which docker`:/usr/bin/docker \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /lib/x86_64-linux-gnu/:/lib/x86_64-linux-gnu/ \
-    -v /root/bin \
     -v `pwd`:/ceph "$@"
 }
 

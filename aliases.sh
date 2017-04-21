e0e1ba42919388a64a695d2822d3e5f656c2d67b
Ivo Jimenez <ivo.jimenez@gmail.com>
2017-04-20 18:09:12 -0700

    Adds OSX support for aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -2,15 +2,24 @@
 #  * add way of identifying cephbuilder image tag based on HEAD
 #  * if no args given, select ivotron/cephbuilder:jewel
 function dmake {
-  docker_path=$(which docker)
-  libltdl_path=$(ldd /usr/bin/docker | grep libltdl | awk '{print $3}')
-
-  if [ -n "$libltdl_path" ] ; then
-    libltdl_path="--volume $libltdl_path:/usr/lib/$(basename $libltdl_path)"
+  type docker >/dev/null 2>&1 || { echo >&2 "Can't find docker command."; exit 1; }
+  docker_path=""
+  if [ $OSTYPE == "linux-gnu" ] ; then
+    docker_path=`which docker`
+    libltdl_path=`ldd $docker_path | grep libltdl | awk '{print $3}'`
+      if [ -n "$libltdl_path" ] ; then
+      libltdl_path="--volume $libltdl_path:/usr/lib/`basename $libltdl_path`"
+      fi
+  elif  [[ $OSTYPE == *"darwin"* ]]; then
+    docker_path="/usr/bin/docker"
+    libltdl_path=""
+  else
+    echo "Unrecognized OS: $OSTYPE"
+    exit 1
   fi
 
   docker run --rm $libtdl_path \
-    -v `which docker`:/usr/bin/docker \
+    -v $docker_path:/usr/bin/docker \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v `pwd`:/ceph "$@"
 }
